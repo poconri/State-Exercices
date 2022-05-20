91c57bbcb46d14219d18b9c68756e7db057af59c
Ramon <pocon.ri@gmail.com>
2022-05-20 16:56:23 -0600

    correcting typescript bug

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import './App.css';
 
 interface InitialState {
   counter: number;
-  list: Character[] | [];
+  list: Character[];
   error: string;
   loading: boolean;
+  favorite: Character[];
 }
 
 interface Character {
@@ -26,19 +27,19 @@ interface Character {
 }
 
 type ActionType = {
-  type: 'FETCH_CHARACTERS' | 'FETCH_CHARACTERS_SUCCESS' | 'FETCH_CHARACTERS_ERROR';
-  payload?: Character[] | boolean | string;
+  type: 'FETCH_CHARACTERS' | 'FETCH_CHARACTERS_SUCCESS' | 'FETCH_CHARACTERS_ERROR' | 'FETCH_CHARACTERS_NEXT' | 'FETCH_CHARACTERS_PREVIOUS' | 'ADD_FAVORITE';
+  payload: Character[] | Character |boolean | string;
 }
 
 const initialState: InitialState = {
   counter: 1,
   list: [],
   error: '',
-  loading: false
+  loading: false,
+  favorite: [],
 };
 
 function reducerRick(state: InitialState, action: ActionType) {
-  console.log(state,"estado dentro de reducer");
   switch (action.type) {
     case 'FETCH_CHARACTERS':
       return {
@@ -46,18 +47,46 @@ function reducerRick(state: InitialState, action: ActionType) {
         loading: true
       };
     case 'FETCH_CHARACTERS_SUCCESS':
-      console.log(state, action, "estado dentro del fetch")
       return {
         ...state,
-        list: action.payload,
-        loading: false
+        list: action.payload as Character[],
+        loading: false,
       };
     case 'FETCH_CHARACTERS_ERROR':
       return {
         ...state,
-        error: action.payload,
+        error: action.payload as string,
         loading: false
       };
+    case 'FETCH_CHARACTERS_NEXT':
+      if(state.counter < 43) {
+        return {
+          ...state,
+          counter: state.counter + 1,
+          error: '',
+        };
+      } else {
+        return {
+          ...state,
+          error: 'No more characters'
+        };
+      }      
+    case 'FETCH_CHARACTERS_PREVIOUS':
+      if(state.counter > 1) {return {
+        ...state,
+        counter: state.counter - 1,
+        error: '',
+      };} else {
+        return {
+          ...state,
+          error: 'No more characters'
+        };
+      }
+    case 'ADD_FAVORITE': 
+      return {
+        ...state,
+        favorite: [...state.favorite, action.payload as Character]
+      }
     default:
       return state;
   }
@@ -69,8 +98,7 @@ function App() {
 
   useEffect(() => {
 
-    console.log(state, "estado dentro de useEffect");
-    const fetchData = async (page:number ): Promise<unknown> => {
+    const fetchData = async (page:number ): Promise<Character[] | void > => {
       try {
         const result = await fetch(`https://rickandmortyapi.com/api/character/?page=${state.counter}`);
         const data = await result.json();
@@ -86,10 +114,25 @@ function App() {
 
   }, [state.counter]);
 
+  const fetchNext = () => {
+    dispatch({
+      type: 'FETCH_CHARACTERS_NEXT',
+      payload: true,
+    });
+  }
+
+  const fetchPrevious = () => {
+    dispatch({
+      type: 'FETCH_CHARACTERS_PREVIOUS',
+      payload: true,
+    });
+  }
+
   return (
     <div style={{background:'gray', height:'100vh', width:'100vw'}}>
-      <button onClick={()=>{}}>Previous</button>
-      <button onClick={()=>{}}>Next</button>
+      <button onClick={fetchPrevious}>Previous</button>
+      <button onClick={fetchNext}>Next</button>
+      {state.error && <p>{state.error}</p>}
       {state.list.map((character: Character) => (
         <div key={character.id} style={{background:'white', height:'350px', width:'200px', margin:'10px'}}>
           <img src={character.image} alt={character.name}/>
